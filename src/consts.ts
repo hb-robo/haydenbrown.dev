0481a2ee056df15144eddd140436be8f40238919
Hayden T. Brown <26484694+hb-robo@users.noreply.github.com>
2025-03-03 09:16:19 -0500

    Revert "v1.0 - site launch (#1)"
    This reverts commit 336d095d549ce57b128c684afbab92cfff43523b.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'haydenbrown.dev';
-export const SITE_DESCRIPTION = 'A digital space for my projects and thoughts';
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
