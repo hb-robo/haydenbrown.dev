12d9ee33b9a089db56a30320576ba7061ec357ee
Hayden T. Brown <26484694+hb-robo@users.noreply.github.com>
2025-03-04 22:44:45 -0500

    fix: small site branding quirks to improve RSS feed metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'haydenbrown.dev';
-export const SITE_DESCRIPTION = 'A digital space for my projects and thoughts';
+export const SITE_TITLE = 'Hayden T. Brown';
+export const SITE_DESCRIPTION = 'Chronicling experiences with art, tech, and the world';
