f448f16f07befab892e07c478ea2e984d3ee94bb
hb-robo <brownhayden10@gmail.com>
2025-03-04 22:43:19 -0500

    feat: fully functional RSS feed for blog history

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,13 @@
-import { glob } from 'astro/loaders';
+import { rssSchema } from '@astrojs/rss';
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `content/blog/` directory.
 	loader: glob({ base: './content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
+	schema: rssSchema.extend({
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		tags: z.array(z.string()).optional(), // Adding tags as an array of strings
 	}),
 });
 
